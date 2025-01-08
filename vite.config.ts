6bf472fbe5661911be82b4326fd609d72723b3c6
SebLOURDEL <seb.lourdel8@gmail.com>
2025-01-08 16:00:07 +0100

    fix: update vercel and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import type { UserConfig } from 'vite'
 
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-
-    }
-  },
   build: {
     minify: 'terser',
     terserOptions: {
       compress: {
         drop_console: true,
         drop_debugger: true,
-        pure_funcs: ['console.log']
+        pure_funcs: ['console.log'] as string[]
       }
     },
     rollupOptions: {
@@ -25,6 +21,8 @@ export default defineConfig({
           'lucide': ['lucide-vue-next']
         }
       }
-    }
+    },
+    chunkSizeWarningLimit: 1600,
+    sourcemap: true
   }
-})
+}) as UserConfig
